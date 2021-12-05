29d218be2a27009322596ad66bd655db704c227d
mustafa-shaikh <wohikhudahe@gmail.com>
2021-12-05 15:04:11 +0500

    0.0.1 Template Code

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,16 +9,9 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import Colors from './Colors';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
+const Section = ({ children, title }): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -65,25 +50,13 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="IPT Project">
+            Capture or Select <Text style={styles.highlight}>Image</Text> from Gallery to Extract the <Text style={styles.highlight}>Text</Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
---
+++ Colors.js
@@ -0,0 +1,12 @@
+
+let Colors = {
+    "black": "#000", 
+    "dark": "#444", 
+    "darker": "#222", 
+    "light": "#DAE1E7", 
+    "lighter": "#F3F3F3", 
+    "primary": "#1292B4", 
+    "white": "#FFF"
+};
+
+export default Colors;
